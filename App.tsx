fc9adfed4ff59a2705a29bb4cad853a16d391007
Bakare Abiola <iamstarcode@gmail.com>
2023-01-08 07:47:33 +0100

    updated dependencies

--- App.tsx
+++ App.tsx
@@ -1,44 +1,40 @@
-import { useCallback, useEffect, useState, useContext } from 'react';
+import { useCallback, useEffect, useState, useContext } from "react";
 
-import { Provider } from 'react-redux'
+import { Provider } from "react-redux";
 
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaView } from "react-native-safe-area-context";
 
-
-import { StatusBar } from 'expo-status-bar';
-import * as SplashScreen from 'expo-splash-screen';
-import * as Font from 'expo-font';
+import { StatusBar } from "expo-status-bar";
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
 
 import { Box, NativeBaseProvider, View } from "native-base";
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import { theme } from './config/native-base-config';
+import { theme } from "./config/native-base-config";
 
-import { AuthProvider } from './context/AuthContext'
-import { AxiosProvider } from './context/AxiosContext';
+import { AuthProvider } from "./context/AuthContext";
+import { AxiosProvider } from "./context/AxiosContext";
 
-import { NBBox } from './components/ui/NBBox';
+import { NBBox } from "./components/ui/NBBox";
 
 //Screens
-import SignIn from './screens/auth/signin'
-import SingUp from './screens/auth/signup';
-import Home from './screens/home';
+import SignIn from "./screens/auth/signin";
+import SingUp from "./screens/auth/signup";
+import Home from "./screens/home";
 
-
-
-import { RootStackParamList } from './types';
-import useAuth from './hooks/useAuth';
-import { useAppSelector } from './app/hooks';
-import { selectAuthenticated } from './app/features/auth/authSlice';
-
+import { RootStackParamList } from "./types";
+import useAuth from "./hooks/useAuth";
+import { useAppSelector } from "./app/hooks";
+import { selectAuthenticated } from "./app/features/auth/authSlice";
+import React from "react";
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 export default function App() {
-
   const [appIsReady, setAppIsReady] = useState(false);
-  const authenticated = useAppSelector(selectAuthenticated)
+  const authenticated = useAppSelector(selectAuthenticated);
 
   useEffect(() => {
     async function prepare() {
@@ -49,10 +45,10 @@ export default function App() {
 
         //Check for refresh token if any, continue until next until next API request hits
         await Font.loadAsync({
-          'EuclidCircularARegular': require('./assets/fonts/euclid-circular-a-cufonfonts/Euclid-Circular-A-Regular.ttf'),
-          'EuclidCircularABold': require('./assets/fonts/euclid-circular-a-cufonfonts/Euclid-Circular-A-Bold.ttf'),
-          'EuclidCircularAMedium': require('./assets/fonts/euclid-circular-a-cufonfonts/Euclid-Circular-A-Medium.ttf'),
-          'EuclidCircularALight': require('./assets/fonts/euclid-circular-a-cufonfonts/Euclid-Circular-A-Light.ttf'),
+          EuclidCircularARegular: require("./assets/fonts/euclid-circular-a-cufonfonts/Euclid-Circular-A-Regular.ttf"),
+          EuclidCircularABold: require("./assets/fonts/euclid-circular-a-cufonfonts/Euclid-Circular-A-Bold.ttf"),
+          EuclidCircularAMedium: require("./assets/fonts/euclid-circular-a-cufonfonts/Euclid-Circular-A-Medium.ttf"),
+          EuclidCircularALight: require("./assets/fonts/euclid-circular-a-cufonfonts/Euclid-Circular-A-Light.ttf"),
         });
         //get refreshToken
       } catch (e) {
@@ -83,27 +79,44 @@ export default function App() {
 
   return (
     <AxiosProvider>
-        <NativeBaseProvider theme={theme}>
-          <NavigationContainer linking={{ prefixes: ['http://localhost:19006/'] }}>
-            <Box onLayout={onLayoutRootView} flex={1} bg="coolGray.100" safeArea>
-              <StatusBar style="inverted" animated={true} backgroundColor="#3333334a" translucent={true} />
-              <Stack.Navigator>
-                {authenticated === true ?
-                  (<>
-                    <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
-                  </>
-                  )
-                  : (<>
-                    <Stack.Screen name="SignIn" component={SignIn} options={{ headerShown: false }} />
-                    <Stack.Screen name="SignUp" component={SingUp} options={{ headerShown: false }} />
-                  </>
-                  )
-                }
-              </Stack.Navigator>
-            </Box>
-          </NavigationContainer>
-        </NativeBaseProvider>
+      <NativeBaseProvider theme={theme}>
+        <NavigationContainer
+          linking={{ prefixes: ["http://localhost:19006/"] }}
+        >
+          <Box onLayout={onLayoutRootView} flex={1} bg="coolGray.100" safeArea>
+            <StatusBar
+              style="inverted"
+              animated={true}
+              backgroundColor="#3333334a"
+              translucent={true}
+            />
+            <Stack.Navigator>
+              {authenticated === true ? (
+                <>
+                  <Stack.Screen
+                    name="Home"
+                    component={Home}
+                    options={{ headerShown: false }}
+                  />
+                </>
+              ) : (
+                <>
+                  <Stack.Screen
+                    name="SignIn"
+                    component={SignIn}
+                    options={{ headerShown: false }}
+                  />
+                  <Stack.Screen
+                    name="SignUp"
+                    component={SingUp}
+                    options={{ headerShown: false }}
+                  />
+                </>
+              )}
+            </Stack.Navigator>
+          </Box>
+        </NavigationContainer>
+      </NativeBaseProvider>
     </AxiosProvider>
   );
 }
-
